f42cefb738d6c757a67589a23b8037dba9a8b8f2
NataliaLaverycheva <ll.natasha@mail.ru>
2014-07-04 04:47:19 +1100

    Добавлена процедура получения id конфликтующих записей

--- conflicts.pas
+++ conflicts.pas
@@ -5,15 +5,24 @@ unit Conflicts;
 interface
 
 uses
-  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ComCtrls;
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ComCtrls,
+  Connect, sqldb, DB;
 
 type
+
+  TConflictId = record
+    Id1, Id2: integer;
+  end;
 
   { TConflictForm }
 
   TConflictForm = class(TForm)
+    SQLQuery: TSQLQuery;
     TreeView: TTreeView;
+    procedure FormCreate(Sender: TObject);
   private
+    ConflictId: array of array of TConflictId;
+    procedure GetDataConflict;
     { private declarations }
   public
     ConflictsName: array of string;
@@ -35,11 +44,12 @@ begin
   ConflictQuery[High(ConflictQuery)] := AQuery;
 end;
 
-procedure TConflictForm.GetTree;
-var
-  i, j: integer;
-  MainTreeNode, ConfTreeNode, ChildConfTreeNode: TTreeNode;
+procedure TConflictForm.FormCreate(Sender: TObject);
 begin
+  SQLQuery := TSQLQuery.Create(Self);
+  SQLQuery.DataBase := ConnectForm.IBConnection;
+  SQLQuery.Transaction := ConnectForm.SQLTransaction;
+
   SetLength(ConflictsName, Length(ConflictQuery));
   ConflictsName[0] := 'Разные пары в одной аудитории';
   ConflictsName[1] := 'Преподаватель в разных аудиториях';
@@ -47,16 +57,46 @@ begin
   ConflictsName[3] := 'Группа в разных аудиториях';
   ConflictsName[4] := 'Группа на разных парах';
   ConflictsName[5] := 'Дублирующиеся пары';
+end;
 
+procedure TConflictForm.GetDataConflict;
+var
+  i: integer;
+  ConfId: TConflictId;
+begin
+  SetLength(ConflictId, Length(ConflictQuery));
+  for i := 0 to High(ConflictId) do begin
+    with SQLQuery do begin
+      Close;
+      SQL.Text := ConflictQuery[i];
+      Open;
+      First;
+       while not EOF do begin
+         SetLength(ConflictId[i], Length(ConflictId[i]) + 1);
+         ConfId.Id1 := SQLQuery.Fields[0].AsInteger;
+         ConfId.Id2 := SQLQuery.Fields[9].AsInteger;
+         ConflictId[i, High(ConflictId[i])] := ConfId;
+         Next;
+       end;
+    end;
+  end;
+end;
+
+procedure TConflictForm.GetTree;
+var
+  i, j: integer;
+  MainTreeNode, ConfTreeNode, ChildConfTreeNode: TTreeNode;
+begin
+  GetDataConflict;
   MainTreeNode := TreeView.Items.Add(nil, 'Конфликты');
   for i := 0 to High(ConflictQuery) do begin
     ConfTreeNode := TreeView.Items.AddChild(MainTreeNode,
       Format('%d. %s', [i + 1, ConflictsName[i]]));
-      //for j := 0 to 2{High(Pairs)} do begin
-      //  ChildConfTreeNode := TreeView.Items.AddChild(
-      //    ConfTreeNode, Format('Конфликт между %d и %d',
-      //    [, ]));
-      //end;
+      for j := 0 to High(ConflictId[i]) do begin
+        ChildConfTreeNode := TreeView.Items.AddChild(
+          ConfTreeNode, Format('Конфликт между %d и %d',
+          [ConflictId[i, j].Id1, ConflictId[i, j].Id2]));
+      end;
   end;
 end;
 
